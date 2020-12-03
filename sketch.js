112e071ce58a292ba5c39082f78751ec4b3520ea
KanishkaRepo <70097774+KanishkaRepo@users.noreply.github.com>
2020-12-02 23:50:34 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+
+var logo, logoImage ;
+var gameState = "menuScreen";
+var menuIcon, menuImage;
+
+function preload()
+{
+ logoImage = loadImage("images/LOGO.jpeg")
+ menuImage = loadImage("images/menu bar icon.png")
+}
+
+function setup() {
+	createCanvas(650, 650);
+
+
+	//engine = Engine.create();
+	//world = engine.world;
+
+	//Create the Bodies Here.
+	logo = createSprite(250, 300, 700, 600);
+	logo.addImage("logo" ,logoImage);
+	logo.scale = 0.5;
+
+	menuIcon = createSprite(620, 45,10, 10);
+	menuIcon.addImage("icon", menuImage);
+	menuImage.scale = 0.1;
+
+  
+}
+
+
+function draw() {
+  
+  background(rgb(234, 164, 164));
+  
+  if(keyDown("space")){
+    logo.visible = false;
+	gameState = "menuScreen";
+  }
+
+  drawSprites();
+ 
+}
+
+
+
